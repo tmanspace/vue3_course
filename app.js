77f672f7838ec473839970148087fdc674441e6f
Tim <serazutdinov.tim@gmail.com>
2021-08-11 22:50:00 +0300

    Basics 04 - Event binding

--- app.js
+++ app.js
@@ -1,17 +1,11 @@
 const app = Vue.createApp({
     data() {
         return {
-            courseGoal: 'Finish the course and master Vue!',
-            vueLink: 'https://vuejs.org/'
+            counter: 0,
         }
     },
     methods: {
-        outputGoal() {
-            const randomNum = Math.random();
-            if (randomNum < 0.5) return 'Learn Vue!';
-            else return 'Master Vue!'
-        }
     }
 });
 
-app.mount('#user-goal');
+app.mount('#events');
