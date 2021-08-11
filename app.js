4b2d4c5460bc3e23e9ff85a87f32b1b6978a4847
Tim <serazutdinov.tim@gmail.com>
2021-08-11 22:53:07 +0300

    Basics 05 - Events & Methods

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const app = Vue.createApp({
         }
     },
     methods: {
+        add() {
+            this.counter++;
+        },
+        reduce() {
+            this.counter--;
+        }
     }
 });
 
