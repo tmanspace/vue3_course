f3410f703350f362ac1b1822a253804df32e1377
Tim <serazutdinov.tim@gmail.com>
2021-11-22 21:52:12 +0300

    42. Styling practice

--- app.js
+++ app.js
@@ -1,56 +1,18 @@
 const app = Vue.createApp({
     data() {
         return {
-            counter: 0,
-            name: '',
-            lastName: '',
+            color: '',
+            visible: true,
+            classWritten: ''
         }
     },
     watch: {
-        result() {
-            const that = this;
-            setTimeout( function() {
-                that.counter = 0;
-            }, 5000);
-            
-        }
-        // method(newValue, oldValue) uneffective if 2 much deps
-        // name(val) {
-        //     if (val == '') this.fullname = '';
-        //     this.fullname = val + ' Serazutdinov';
-        // }
     },
     computed: {
-        fullname() {
-            console.log('Running...') // Bad solution
-             if (!this.name) {}
-             else return this.name + ' ' + this.lastName;
-        },
-        result() {
-            if (this.counter < 37) {
-                return 'Not yet!';
-            } else if (this.counter == 37) {
-                return 'Exactly';
-            } else return 'Too much';
-        }
     },
     methods: {
-        // outputFullname() {
-        //     console.log('Running...') // Bad solution
-        //     if (!this.name) {}
-        //     else return this.name + ' ' + this.surname;
-        // },
-        add(num) {
-            this.counter += num;
-        },
-        reduce(num) {
-            this.counter -= num;
-        },
-        submitForm() {
-            alert('Form submitted');
-        },
-        resetInput() {
-            this.name = ''; 
+        toggleVis() {
+            this.visible = !this.visible;
         }
     }
 });
